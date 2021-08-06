e26cee5473f3b465974efc20b2db10c02c2684f9
Charles Tapley Hoyt <cthoyt@gmail.com>
2021-08-06 11:15:51 +0200

    Add first iteration of check script
    Still want to get all issues and calculate some statistics about responsiveness

---
+++ build.py
@@ -0,0 +1,145 @@
+import datetime
+import pathlib
+from typing import Iterable
+
+import dateparser
+import pandas as pd
+import pystow
+import requests
+import yaml
+from tqdm import tqdm
+
+HERE = pathlib.Path(__file__).parent.resolve()
+DATA = HERE.joinpath("_data")
+DATA.mkdir(exist_ok=True, parents=True)
+
+URL = "https://raw.githubusercontent.com/OBOFoundry/OBOFoundry.github.io/master/_config.yml"
+PREFIX = "https://github.com/"
+TITLE = "Add obofoundry topic to repo metadata"
+
+# Load the GitHub access token via PyStow. We'll
+# need it so we don't hit the rate limit
+TOKEN = pystow.get_config("github", "token", raise_on_missing=True)
+
+NOW = datetime.datetime.now()
+ONE_YEAR_AGO = NOW - datetime.timedelta(weeks=52)
+
+
+def get_most_recent_closed_issue(owner, repo):
+    issues = get_issues(
+        owner, repo, params={"state": "closed", "sort": "closed", "direction": "desc"}
+    )
+    if issues:
+        return issues[0]
+
+
+def get_oldest_open_issue(owner, repo):
+    issues = get_issues(
+        owner, repo, params={"state": "open", "sort": "updated", "direction": "asc"}
+    )
+    if issues:
+        return issues[0]
+
+
+def get_issues(owner, repo, params=None):
+    return get_github(
+        f"https://api.github.com/repos/{owner}/{repo}/issues", params=params
+    )
+
+
+def get_info(owner, repo):
+    return get_github(f"https://api.github.com/repos/{owner}/{repo}")
+
+
+def get_topics(owner, repo):
+    # Get all topics from the repository. See more information on the GH docs:
+    # https://docs.github.com/en/rest/reference/repos#get-all-repository-topics
+    rv = get_github(
+        f"https://api.github.com/repos/{owner}/{repo}/topics",
+        accept="application/vnd.github.mercy-preview+json",
+    )
+    return rv["names"]
+
+
+def get_github(url, accept=None, params=None):
+    headers = {
+        "Authorization": f"token {TOKEN}",
+    }
+    if accept:
+        headers["Accept"] = accept
+    return requests.get(url, headers=headers, params=params).json()
+
+
+def iterate_repos() -> Iterable[tuple[str, str]]:
+    res = requests.get(URL)
+    res = yaml.safe_load(res.content)
+
+    for record in tqdm(res["ontologies"], desc="Processing OBO conf"):
+        tracker = record.get("tracker")
+
+        # All active ontologies have trackers. Most of them
+        # have GitHub, but not all. Don't consider the non-GitHub
+        # ones
+        if not tracker or not tracker.startswith(PREFIX):
+            continue
+
+        # Since we assume it's a github link, slice out the prefix then
+        # parse the owner and repository out of the path
+        owner, repo, *_ = tracker[len(PREFIX) :].split("/")
+        yield owner, repo
+
+
+def main(force: bool = False) -> None:
+    repos = sorted(iterate_repos())
+    rv = []
+    repos = tqdm(repos, desc="Repositories")
+    for owner, repo in repos:
+        repos.set_postfix(repo=f"{owner}/{repo}")
+        info = get_info(owner, repo)
+        description = info["description"]
+        stars = info["stargazers_count"]
+        license = info["license"]
+        open_issues = info["open_issues"]
+        homepage = info["homepage"]
+        pushed_at = info["pushed_at"]  # datetime of last push
+        pushed_last_year = ONE_YEAR_AGO < dateparser.parse(info["pushed_at"]).replace(
+            tzinfo=None
+        )
+        topics = get_topics(owner, repo)
+        has_obofoundry_topic = "obofoundry" in topics
+        if (
+            most_recent_closed_issue := get_most_recent_closed_issue(owner, repo)
+        ) is not None:
+            last_close_datetime = most_recent_closed_issue["closed_at"]
+            last_close_number = most_recent_closed_issue["number"]
+            last_close_last_year = ONE_YEAR_AGO < dateparser.parse(
+                last_close_datetime
+            ).replace(tzinfo=None)
+        else:
+            last_close_datetime = None
+            last_close_number = None
+            last_close_last_year = False
+
+        # when was the last issue closed?
+        rv.append(
+            dict(
+                description=description,
+                stars=stars,
+                license=license["key"] if license else None,
+                open_issues=open_issues,
+                homepage=homepage,
+                pushed_at=pushed_at,
+                pushed_last_year=pushed_last_year,
+                has_obofoundry_topic=has_obofoundry_topic,
+                last_close_datetime=last_close_datetime,
+                last_close_number=last_close_number,
+                last_close_last_year=last_close_last_year,
+            )
+        )
+    pd.DataFrame(rv).to_csv(DATA.joinpath("data.tsv"), sep="\t", index=False)
+    with DATA.joinpath("data.yml").open("w") as file:
+        yaml.safe_dump(rv, file)
+
+
+if __name__ == "__main__":
+    main()
